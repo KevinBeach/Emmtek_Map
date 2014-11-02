25c6f645ff3145ed464450cea19f894384cac87a
Kevin Beach <kevin.beach@hotmail.co.uk>
2014-11-02 01:20:32 +0000

    Added in google map fragment - need to check enable for google maps as fails

--- app/src/main/java/com/keseni/emmtek_map/MapActivity.java
+++ app/src/main/java/com/keseni/emmtek_map/MapActivity.java
@@ -3,6 +3,8 @@ package com.keseni.emmtek_map;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,8 +13,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MapActivity extends Activity {
+
+    private static final String MAP_FRAGMENT_TAG = "map";
+    private GoogleMap mMap;
+    private MapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,33 @@ public class MapActivity extends Activity {
                     .add(R.id.bottom_frame, new Bottom_Fragment_Button())
                     .commit();
         }
+        mMapFragment = (MapFragment) getFragmentManager().findFragmentByTag(MAP_FRAGMENT_TAG);
+        if(mMapFragment == null){
+            mMapFragment = MapFragment.newInstance();
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.top_frame,mMapFragment,MAP_FRAGMENT_TAG);
+            fragmentTransaction.commit();
+        }
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if(mMap == null){
+            mMap = mMapFragment.getMap();
+            if (mMap != null){
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("marker"));
     }
 
 

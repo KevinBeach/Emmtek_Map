a70c2ca73daea91810e0b589d301e88295aa8bc3
Kevin Beach <kevin.beach@hotmail.co.uk>
2015-02-21 17:44:16 +0000

    Branch takes in Server Push data, writes it to preferences Map latlon works OK now (needs further testing) Map now updates on shared preference update Markers on map are removed when location changes Added in a settings dialog box and a new geofence menu option (not yet implemented geofence dialog box). Settings dialog box writes and reads from preferences and has been tested Notification added, but basic - no configuration control yet, activation when app is only not in focus etc. Basic notifcation tested.
    Issues
    
    Server push data doesn't match example given (data description is for 25 elements, example gives 27). Need to amend comma count as applicable.
    Change Location button to give a server push instead of locate
    
    Features to be added
    
    Notifications (add notification if app isn't in focus - use boolean preference to manage infocus)
    Set up geo-fencing, get broadcast receiver to notify when set off
    Set up and manage alarms via menus

--- app/src/main/java/com/keseni/emmtek_map/SmsReceiver.java
+++ app/src/main/java/com/keseni/emmtek_map/SmsReceiver.java
@@ -1,10 +1,15 @@
 package com.keseni.emmtek_map;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
@@ -67,7 +72,21 @@ public class SmsReceiver extends BroadcastReceiver {
                                 Toast.makeText(context, "Server Push Received on " + senderPhoneNumber.toString(),Toast.LENGTH_SHORT).show();
                             EmmTekServerData emmTekServerData = new EmmTekServerData(messageReceived);
                             Boolean write_done = Write_To_Preferences(emmTekServerData);
+                            
+                            // Now set up a notification for a server push
+                            // First set up pending intent
 
+                            Intent i = new Intent(context,MapActivity.class);
+                            PendingIntent pi = PendingIntent.getActivity(context,0,i,0);
+                            Notification notification = new NotificationCompat.Builder(context)
+                                    .setTicker("Emmtek Data Received")
+                                    .setSmallIcon(R.drawable.ic_launcher)
+                                    .setContentTitle("SMS received")
+                                    .setContentIntent(pi)
+                                    .setAutoCancel(true)
+                                    .build();
+                            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                            notificationManager.notify(0,notification);
                         }
                     }
                 }
